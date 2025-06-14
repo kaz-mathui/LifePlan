340d51596115fa5c59d08345ca4abfe10bda1166
kazmathui <46832172+kaz-mathui@users.noreply.github.com>
2025-06-15 07:34:51 +0900

    10pipelinetest

---
+++ backend/diag.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "--- [DIAGNOSIS] Script started at $(date). ---"
+echo "--- [DIAGNOSIS] Printing environment variables: ---"
+printenv
+echo "--- [DIAGNOSIS] Script finished successfully at $(date). ---"
+exit 0 
