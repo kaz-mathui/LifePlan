e8a16ddaff31c2e3218f0f9097e0accaa11e938c
kazmathui <46832172+kaz-mathui@users.noreply.github.com>
2025-06-15 07:59:26 +0900

    １２pipiline

--- backend/diag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "--- [DIAGNOSIS] Script started at $(date). ---"
-echo "--- [DIAGNOSIS] Printing environment variables: ---"
-printenv
-echo "--- [DIAGNOSIS] Script finished successfully at $(date). ---"
-exit 0 
